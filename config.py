c38befdbdee229e7a30201429bb5b5c481ba2e5a
Manish0139 <manishkumar1295mk1309@gmail.com>
2025-05-22 20:16:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "29092089"
 api_hash = "28d4561d1b13c8600282456c9898d909"
-bot_token = "7261277639:AAG1yziYZfyVi0UwAeWbR5kpHUqajdrOPKM"
-auth_users = [7939644457]
+bot_token = ""
+auth_users = [7877249692]
